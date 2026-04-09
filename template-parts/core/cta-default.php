9446cce571ab7a3880cbd39daf5ca72774e493c9
AdviceDamy <damy@advice.nl>
2026-04-09 10:35:50 +0200

    Update _base.css to adjust font size for .body-medium and p elements, enhancing responsiveness. Modify cta-default.php to improve layout by increasing padding and setting a maximum width for the title, ensuring better visual consistency in call-to-action sections.

--- template-parts/core/cta-default.php
+++ template-parts/core/cta-default.php
@@ -7,9 +7,9 @@ include locate_template('template-parts/core/cta-get-data.php');
     <div class="container">
         <div class="w-full lg:w-10/12 mx-auto px-0 lg:px-[65px] overflow-hidden relative">
             <div class="flex flex-col items-center lg:flex-row border border-[rgba(22,22,22,0.12)] rounded-[20px] p-[20px] lg:p-[60px]">
-            <div class="w-full lg:w-6/12 order-1 lg:order-2 relative mb-[24px] lg:mb-0! lg:pr-[60px]">
+            <div class="w-full lg:w-6/12 order-1 lg:order-2 relative mb-[24px] lg:mb-0! lg:pr-[160px]">
                 <?php if (!empty($cta_titel)): ?>
-                    <h3 class="headline-small mt-0! mb-0!"><?php echo $cta_titel; ?></h3>
+                    <h3 class="headline-small mt-0! mb-0! max-w-[420px]"><?php echo $cta_titel; ?></h3>
                 <?php endif; ?>
                 <?php if (!empty($cta_content)): ?>
                     <div class="opacity-80"><?php echo $cta_content; ?></div>
