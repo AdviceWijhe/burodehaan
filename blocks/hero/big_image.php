77d5787ac9c3c8e4924c9b65fa6ff8abf7ae7e00
AdviceCalvin <calvin@advice.nl>
2026-04-15 11:34:03 +0200

    Update hero block styles and typography for improved layout and readability
    - Adjust height settings for the hero image container to enhance responsiveness.
    - Update padding and margin classes for better alignment in the hero content.
    - Modify font sizes for various headings and labels to ensure better scalability and visual hierarchy.

--- blocks/hero/big_image.php
+++ blocks/hero/big_image.php
@@ -1,5 +1,5 @@
 <section class="hero hero__big_image overflow-hidden relative w-full max-w-full">
-    <div class="relative flex items-end w-full p-0 aspect-[1728/750]">
+    <div class="relative flex items-end w-full p-0 h-[600px] md:h-auto md:aspect-[1728/750]">
         <div class="absolute inset-0 h-full w-full">
             <div class="absolute top-0 left-0 w-1/2 h-full" style="background: linear-gradient(90deg, rgba(22, 22, 22, 0.5) 0%, rgba(22, 22, 22, 0) 100%); z-index: 2;"></div>
 <div class="absolute bottom-0 left-0 w-full h-[300px]" style="opacity: 0.5;
@@ -64,12 +64,12 @@ background: linear-gradient(0deg, #0A2031 0%, rgba(10, 32, 49, 0.00) 100%); z-in
             <?php endif; ?>
         </div>
         <div class="w-full relative z-2">
-            <div class="w-full lg:w-6/12 lg:px-[60px] lg:py-[60px] p-[20px] pb-[20px]">
+            <div class="w-full lg:w-6/12 p-[16px] lg:px-[60px] lg:py-[60px]">
                 <?php
                 $hero_title = (string) get_sub_field('titel', false, false);
                 $hero_title = preg_replace('/^\s*<p>(.*)<\/p>\s*$/si', '$1', $hero_title);
                 ?>
-                <div class="text-white !text-white [&_*]:!text-white"><?php echo wp_kses_post($hero_title); ?></div>
+                <div class="headline-large text-white !text-white [&_*]:!text-white"><?php echo wp_kses_post($hero_title); ?></div>
                 <div class="mt-[32px] hero-big-image-buttons">
                 <?= get_template_part('template-parts/core/buttons', null, array('buttons' => get_sub_field('buttons'), 'align_items' => 'start')) ?>
                 </div>
